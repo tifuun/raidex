6e33af4adb270efb92452892befd12582d006413
maybetree <maybetree48@proton.me>
2023-11-24 13:34:55 +0100

    Better popout effect

--- pycif_browser/themes/FirstLight/basedir/browser.js
+++ pycif_browser/themes/FirstLight/basedir/browser.js
@@ -1,48 +1,53 @@
-function toggleExpanded(compo) {
-  /*compo = button.parentElement*/
-  wrap = compo.parentElement
-
-  if (!compo.classList.contains('expanded')) {
-    popOutWidth(wrap);
-    popOut(compo);
+function toggleExpanded(card) {
+  if (!card.classList.contains('expanded')) {
+    blur();
+    popOut(card);
   }
   else {
-    setTimeout(() => {popIn(compo);popInWidth(wrap);}, "320");
+    unblur();
+    setTimeout(() => {popIn(card);}, "540");
     // Timeout gives the animation time to play out
   }
 
-  setTimeout(() => {compo.classList.toggle('expanded');}, "10");
-  //compo.classList.toggle('expanded');
-
+  setTimeout(() => {card.classList.toggle('expanded');}, "10");
 }
 
-function popOut(elem) {
-  // Convert an element's positioning to `absolute`
+function popOut(card) {
+  // Convert an element's positioning to `fixed`
   // while keeping its relative position on screen
-  bbox = elem.getBoundingClientRect();
-  elem.style.position = 'fixed';
-  elem.style.left = bbox.x + "px";
-  elem.style.top = bbox.y + "px";
-  elem.style.width = bbox.width + "px";
-  //elem.style.left = elem.offsetLeft + "px";
-  //elem.style.top = elem.offsetTop + "px";
+  // THANK YOU https://stackoverflow.com/a/77497727/22930104
+  wrap = card.parentElement;
+
+  bbox_wrap = wrap.getBoundingClientRect();
+  wrap.style.width = bbox_wrap.width + "px";
+  wrap.style.height = bbox_wrap.height + "px";
+
+  wrap.style.maxWidth = bbox_wrap.width + "px";
+  wrap.style.minWidth = bbox_wrap.width + "px";
+
+  bbox_card = card.getBoundingClientRect();
+  card.style.position = 'fixed';
+  card.style.left = bbox_card.x - 7 + "px";
+  card.style.top = bbox_card.y - 7 + "px";
+  card.style.width = bbox_card.width + "px";
+  //card.style.maxWidth = bbox_card.width + "px";
+  //card.style.minWidth = bbox_card.width + "px";
 }
 
-function popIn(elem) {
-  elem.style.position = 'relative';
-  elem.style.left = "";
-  elem.style.top = "";
-  elem.style.width = "";
-}
+function popIn(card) {
+  wrap = card.parentElement;
 
-function popOutWidth(elem) {
-  elem.style.width = elem.clientWidth + "px";
-  elem.style.height = elem.clientHeight + "px";
-}
+  card.style.position = 'relative';
+  card.style.left = '';
+  card.style.top = '';
+  card.style.width = '';
+  //card.style.maxWidth = '';
+  //card.style.minWidth = '';
 
-function popInWidth(elem) {
-  elem.style.width = "";
-  elem.style.height = "";
+  wrap.style.width = '';
+  wrap.style.height = '';
+  wrap.style.maxWidth = '';
+  wrap.style.minWidth = '';
 }
 
 // THANK YOU
@@ -64,5 +69,15 @@ function liveSearch(key, child_selector, hide_class) {
       cards[i].classList.add(hide_class);
     }
   }
+}
+
+function blur() {
+  //document.getElementById('blur').classList.add('blur-active');
+  document.body.classList.add('blur-active');
+}
+
+function unblur() {
+  //document.getElementById('blur').classList.remove('blur-active');
+  document.body.classList.remove('blur-active');
 }
 
